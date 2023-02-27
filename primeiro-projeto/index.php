d852e03938d578e1ff6922bc711bd15d5e60ec8d
Vinicius <99505772+vinicius-alves-oliveira@users.noreply.github.com>
2023-02-27 16:45:44 -0300

    Add files via upload

---
+++ primeiro-projeto/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> 
+        <?php
+            echo "ola mundo";
+        ?>
+    </h1>
+
+    <h2>5+5=
+        <?php 
+             echo (5+5);
+        ?>
+    </h2>
+    
+    <h2>4+6=
+        <?php 
+             echo (4+6);
+        ?>
+    </h2>
+    
+    <h2>3*3=
+        <?php 
+             echo (3*3)
+        ?>
+    
+</h2>
+    <h2>10+1=
+        <?php 
+             echo (10+1)
+        ?>
+    </h2>
+    <h2>10+2=
+        <?php 
+             echo (10+2)
+        ?>
+    </h2>
+</body>
+</html>
